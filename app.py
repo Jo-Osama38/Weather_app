02a17a2cfd3b0f1105c6fb9d3b049e004f38a000
Youssef <youssefosamasalah51@gmail.com>
2026-05-28 20:31:55 +0300

    I organized the project's core files ,worte the foundational code for Flask and the HTML page and now i'm learning how to work with the API and how to retrieve data

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask ,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/" ,methods=['GET','POST'])
+def home():
+    city = request.form.get('city')
+    return render_template ('index.html' , city=city)
+
+if __name__ == "__main__":
+    app.run(debug=True)
