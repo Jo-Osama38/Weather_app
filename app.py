a514992e67de6998b0abc8d43e66bcd5b757af91
Youssef <youssefosamasalah51@gmail.com>
2026-05-30 08:00:04 +0300

    I changed the CSS and wrote it almost from scratch to make it more modern I added neo weather information I changed the icons and used PNG images because they look much nicer and are more colojul This took me a lot of time;I used one site for icons and didn't like them so i switched to another site for icons and thern to this PNG .CSS usually doesn't take me this long vut here i think it took this long because i'm trying out many things in one place until i choos one All that's left is to make some final touches to the interface then make the colors and background dynamic, add some animation and the project will be ready

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def home():
         FeelingTemp = None
         humidity = None
         windspeed = None
+        pressure = None
+        clouds= None
+        visibalty = None
         localtime = None
         time = None
         date = None
@@ -31,6 +34,9 @@ def home():
                 FeelingTemp = data["current"]["feelslike_c"]
                 humidity = data["current"]["humidity"]
                 windspeed = data["current"]["wind_kph"]
+                pressure = data["current"]["pressure_mb"]
+                clouds = data["current"]["cloud"]
+                visibalty = data["current"]["vis_km"]
                 localtime = data["location"]["localtime"].split()
                 time = localtime[1]
                 date = localtime[0]
@@ -42,7 +48,7 @@ def home():
 
         return render_template('index.html', city_name=city_name, icon=icon, condition=condition, 
                                 temp=temp, FeelingTemp=FeelingTemp, humidity=humidity, windspeed=windspeed,
-                                    time=time, date=date,error=error)
+                                    time=time, date=date,error=error, pressure=pressure,clouds= clouds, visibalty=visibalty)
 
 if __name__ == "__main__":
     app.run(debug=True)
